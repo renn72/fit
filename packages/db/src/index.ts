b63a421fc0afa128301c81851e19b4ce08e64d5e
renn <renn@warner.systems>
2025-11-14 16:27:22 +1100

    log db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,9 @@
 import Database from 'bun:sqlite'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 
+console.log('database')
+console.log(process.env.DATABASE_URL)
+
 const client = new Database(process.env.DATABASE_URL || '')
 
 export const db = drizzle({ client })
