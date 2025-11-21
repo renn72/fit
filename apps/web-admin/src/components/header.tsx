ccfeed15a488d97cedf2f1dc82a09734d627c232
David <git@warner.systems>
2025-11-21 13:47:54 +1030

    format

--- apps/web-admin/src/components/header.tsx
+++ apps/web-admin/src/components/header.tsx
@@ -1,31 +1,31 @@
-import { Link } from "@tanstack/react-router";
-import { ModeToggle } from "./mode-toggle";
-import UserMenu from "./user-menu";
+import { Link } from '@tanstack/react-router'
+import { ModeToggle } from './mode-toggle'
+import UserMenu from './user-menu'
 
 export default function Header() {
 	const links = [
-		{ to: "/", label: "Home" },
-		{ to: "/dashboard", label: "Dashboard" },
-	] as const;
+		{ to: '/', label: 'Home' },
+		{ to: '/dashboard', label: 'Dashboard' },
+	] as const
 
 	return (
 		<div>
-			<div className="flex flex-row items-center justify-between px-2 py-1">
-				<nav className="flex gap-4 text-lg">
+			<div className='flex flex-row justify-between items-center py-1 px-2'>
+				<nav className='flex gap-4 text-lg'>
 					{links.map(({ to, label }) => {
 						return (
 							<Link key={to} to={to}>
 								{label}
 							</Link>
-						);
+						)
 					})}
 				</nav>
-				<div className="flex items-center gap-2">
+				<div className='flex gap-2 items-center'>
 					<ModeToggle />
 					<UserMenu />
 				</div>
 			</div>
 			<hr />
 		</div>
-	);
+	)
 }
